26153d00888f15fada78a2778608c1881fc4e5e0
fajlajpi <fajlajpi@gmail.com>
2025-09-08 00:52:13 +0200

    Fix: Changed astro config to remove base setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bluefoxtours.cz', // Replace with your site URL
-  base: '/bluefoxtours',
   integrations: [
     tailwind(),
     sitemap(),
